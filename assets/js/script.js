f62c1d8d0e3f21dd799b867c0ae2adb338ae0672
jasonmueller <jmuel9999@gmail.com>
2021-03-28 16:45:25 -0500

    Fixing global variables, adding randomness to questions. Added answer buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,52 @@
 //querySelector variables for DOM manipulation
-var startBtn = document.querySelector("startquiz");
-var pregameContainer = document.querySelector("pregame")
-var quizContainer = document.querySelector("quiz");
-var submitButton = document.querySelector("submit");
-var resultsContainer = document.querySelector("results");
+var startBtn = document.getElementById("startquiz");
+var prequizContainer = document.getElementById("prequiz");
+var quizContainer = document.getElementById("quiz-container");
+//var submitButton = document.getElementById("submit");
+var resultsContainer = document.getElementById("results");
+
+const randomQuestions, currentQuestionIndex;
+
+startBtn.addEventListener("click", startQuiz);
 
 
 //Question array
 var quizQuestions = [
     {questionOne: "Commonly used data types DO NOT include:",
-        answer: {1: "strings", 2: "booleans", 3: "alerts", 4: "numbers"},
+        answers: {1: "strings", 2: "booleans", 3: "alerts", 4: "numbers"},
             correctAnswer: "3"},
     {questionTwo: "The condition of an if/else statement is enclosed with ___________.",
-        answer: {1: "quotes", 2: "curly brackets", 3: "parenthesis", 4: "square brackets"},
+        answers: {1: "quotes", 2: "curly brackets", 3: "parenthesis", 4: "square brackets"},
             correctAnswer: "3"},
     {questionThree: "Arrays in JavaScript can be used to store ____________.",
-        answer: {1: "numbers and strings", 2: "other arrays", 3: "booleans", 4: "all of the above"},
+        answers: {1: "numbers and strings", 2: "other arrays", 3: "booleans", 4: "all of the above"},
             correctAnswer: "4"},
     {questionFour: "String values must be enclosed within _____________ when being assigned to variables.",
-        answer: {1: "commas", 2: "curly brackets", 3: "quotes", 4: "parenthesis"},
+        answers: {1: "commas", 2: "curly brackets", 3: "quotes", 4: "parenthesis"},
             correctAnswer: "3"},
     {questionFive: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answer: {1: "JavaScript", 2: "terminal/bash", 3: "for loops", 4: "console.log"},
+        answers: {1: "JavaScript", 2: "terminal/bash", 3: "for loops", 4: "console.log"},
             correctAnswer: "4"},   
 ];
 
-//array object
-
-
-//start game function
-//document.getElementById(startQuiz);
-//debugger;
 function startQuiz(){
-    
-    
-    var arrayOutput = [];
-    //for ()
-    // for (i = 0; i < quizQuestions.length; i++) {
-    //    if (quizQuestions.questionOne !== 3)
-    //     alert("Wrong!");
-        
-    // }
+    console.log("started");
+    startBtn.classList.add("hide");
+    //shuffles question order
+    randomQuestions = quizQuestions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0;
+    quizContainer.classList.remove("hide");
+    nextQuestion();
 };
 
-//start button
-//document.getElementById(startQuiz);
-startBtn.onclick = startQuiz;
+function nextQuestion(){
+    showQuestion(randomQuestions[currentQuestionIndex])
+};
+
+function showQuestion(question){
+
+};
+
+function selectAnswer(){
+
+};
