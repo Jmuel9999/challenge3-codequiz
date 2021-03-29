b66dcee96af686977447ceced29c8b7eed4204d9
jasonmueller <jmuel9999@gmail.com>
2021-03-28 22:47:35 -0500

    Added working timer that initiates with start button. Fixed but with right and wrong answer button colors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,27 @@
 var startBtn = document.getElementById("startbtn");
 var prequizContainer = document.getElementById("prequiz");
 var quizContainer = document.getElementById("quiz-container");
-//var submitButton = document.getElementById("submit");
 var resultsContainer = document.getElementById("results");
 var questionEl = document.getElementById("question");
 var answerEl = document.getElementById("answerbtns");
+// var answerA = document.getElementById("a")
+// var answerA = document.getElementById("b")
+// var answerA = document.getElementById("c")
+// var answerA = document.getElementById("d")
 //var questionCont = document.getElementById("question-container")
 //var btnHide = document.getElementById("ansbtn");
 var nextButton = document.getElementById("nextbtn");
+var timer = document.getElementById("timer");
 
 let randomQuestions, currentQuestionIndex;
 
+//starts quiz and timer
+startBtn.addEventListener("click", startTimer);
 startBtn.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+    qIndex++
+    nextQuestion();
+});
 
 
 // //Question array
@@ -63,11 +73,8 @@ function startQuiz(){
     //shuffles question order
     randomQuestions = quizQuestions.sort(() => Math.random() - .5)
     qIndex = 0;
-    //debugger;
     quizContainer.classList.remove("hide");
-    //questionCont.classList.remove("hide");
     answerEl.classList.remove("hide");
-    //btnHide.classList.remove("hide"); only brough choice a back
     nextQuestion();
 };
 
@@ -95,9 +102,66 @@ function resetQuestion(){
     nextbtn.classList.add("hide")
     while (answerEl.firstChild) {
         answerEl.removeChild(answerEl.firstChild)
-    }
-}
+    };
+};
 
 function pickAnswer(event){
-
+    var pickBtn = event.target
+    var correct = pickBtn.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    });
+    if (randomQuestions.length > qIndex + 1) {
+        nextbtn.classList.remove("hide")
+    } else{
+        startBtn.innerText = "Restart"
+        startBtn.classList.remove("hide")
+    };
 };
+//colors the buttons according to getting them right or wrong
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("incorrect")
+    };
+};
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("incorrect")
+};
+
+// function wrongAnswer (){
+
+// }
+
+//timer code start
+var timeIntervalUp;
+var secondsDown = 60;
+function startTimer(){
+    timeIntervalUp = setInterval(function(){countTimer()}, 1000);
+};
+function countTimer() {
+    document.getElementById("timer").innerHTML = "Time Remaining: " + secondsDown;
+    secondsDown--;
+        if (secondsDown === 0) {
+            clearInterval(timeIntervalUp);
+            endTimer();
+            XMLHttpRequestUpload();
+        };
+};
+
+// var count = 5;
+// var counter = setInterval(quizTimer, 1000);
+// function quizTimer(){
+//     document.getElementById("timer").innerHTML = count + " seconds left!";
+//     count--;
+//     //clearInterval(counter);
+//     if (count === 0){
+//         clearInterval(counter);
+//         alert("Game Over!")
+//     }
+// };
